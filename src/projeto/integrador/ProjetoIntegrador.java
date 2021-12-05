b35d8eb3431612012e1b04d3e80dde2ec9d25188
Usuario <Usuario@DESKTOP-V2898J3>
2021-12-05 20:52:37 -0300

    ultima atualização para a apresentação

--- src/projeto/integrador/ProjetoIntegrador.java
+++ src/projeto/integrador/ProjetoIntegrador.java
@@ -11,11 +11,69 @@ package projeto.integrador;
  */
 public class ProjetoIntegrador {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  /*create database NEXTLEVELBD;
+
+    use NEXTLEVELBD;
+
+    create table tb_clientes (
+      id int auto_increment primary key,
+      nome varchar(100),
+      email varchar(200),
+      cpf varchar (20),
+      telefone varchar(30)
+
+    );
+
+    create table tb_enderecos(
+      id int auto_increment primary key,
+      cep varchar(100),
+      rua varchar (255),
+      numero int,
+      complemento varchar (200),
+      bairro varchar (100),
+      cidade varchar (100),
+      uf varchar (2),
+      id_cliente int,
+
+      FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id)
+    );
+
+    create table tb_categorias(
+      id int auto_increment primary key,
+      nome varchar(255)
+    );
+
+    create table tb_formapagamento(
+      id int auto_increment primary key,
+      nome varchar(255)
+    );
+
+    CREATE TABLE tb_produtos (
+      id int auto_increment primary key,
+      nome varchar(100),
+      descricao varchar(500),
+      peso double,
+      id_categoria int,
+      valor double,
+      qtd_estoque int,
+
+      FOREIGN KEY (id_categoria) REFERENCES tb_categorias(id)
+    );
+
+    create table tb_pedidos (
+      id int auto_increment primary key,
+      id_cliente int,
+      id_produto int,
+      id_formapagamento int,
+      valor_pedido double,
+      id_endereco int,
+
+      FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id),
+      FOREIGN KEY (id_produto) REFERENCES tb_produtos(id),
+      FOREIGN KEY (id_formapagamento) REFERENCES tb_formapagamento(id),
+      FOREIGN KEY (id_endereco) REFERENCES tb_enderecos(id)
+    );*/
+
+    
     
 }
